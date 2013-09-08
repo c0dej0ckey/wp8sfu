2955c0e49196686b1a0fd525a8c6ae5a856bf7b5
c0dej0ckey <scottwilliams667@gmail.com>
2013-09-07 18:48:45 -0700

    initial wp8sfu

---
+++ PhoneApp1/PhoneApp1/ProtectedServicesPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace PhoneApp1
+{
+    public partial class ProtectedServicesPage : PhoneApplicationPage
+    {
+        public ProtectedServicesPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ServiceChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string service = (string)sender;
+            ProtectedServicesVM vm = (ProtectedServicesVM)this.DataContext;
+            vm.OpenService(service);
+        }
+    }
+}
---
+++ PhoneApp1/PhoneApp1/ProtectedServicesVM.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Navigation;
+
+namespace PhoneApp1
+{
+    public class ProtectedServicesVM
+    {
+        private List<string> mServices = new List<string>();
+
+        public ProtectedServicesVM()
+        {
+            mServices.Add("webct");
+            mServices.Add("sims");
+            mServices.Add("go sfu");
+            mServices.Add("sfu connect");
+
+
+        }
+
+        public List<string> Services
+        {
+            get { return mServices; }
+        }
+
+        public void OpenService(string service)
+        {
+            NavigationService navigationService = ServiceLocator.GetService<NavigationService>();
+
+            if(service.Equals("webct"))
+            {
+                
+            }
+            else if(service.Equals("sims"))
+            {
+                
+            }
+            else if(service.Equals("go sfu"))
+            {
+                
+            }
+            else if(service.Equals("sfu connect"))
+            {
+                
+            }
+        }
+
+    }
+}
