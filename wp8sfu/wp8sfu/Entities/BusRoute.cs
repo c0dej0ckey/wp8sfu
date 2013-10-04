01e70b40d0089749bbcfc8fcb58f32e890258efe
c0dej0ckey <scottwilliams667@gmail.com>
2013-10-04 11:45:38 -0700

    bus routes are parsing for burnaby

---
+++ wp8sfu/wp8sfu/Entities/BusRoute.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wp8sfu.Entities
+{
+    public class BusRoute
+    {
+        private string mRouteNumber;
+        private string mRouteName;
+        private List<string> mBusRouteTimes;
+
+        public BusRoute(string routeNumber, string routeName)
+        {
+            this.mBusRouteTimes = new List<string>();
+            this.mRouteName = routeName;
+            this.mRouteNumber = routeNumber;
+        }
+
+        public string RouteNumber
+        {
+            get { return this.mRouteNumber; }
+            set { this.mRouteNumber = value; }
+        }
+
+        public string RouteName
+        {
+            get { return this.mRouteName; }
+            set { this.mRouteName = value; }
+        }
+
+        public void AddRouteTime(string time)
+        {
+            mBusRouteTimes.Add(time);
+        }
+
+        public void RemoveRouteTimes()
+        {
+            mBusRouteTimes.Clear();
+        }
+    }
+
+}
--- wp8sfu/wp8sfu/VMs/TransitVM.cs
+++ wp8sfu/wp8sfu/VMs/TransitVM.cs
@@ -1,30 +1,39 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using wp8sfu.Entities;
 
 namespace wp8sfu.VMs
 {
-    public class TransitVM
+    public class TransitVM : INotifyPropertyChanged
     {
-        private static List<string> sBurnabyStops = new List<string>() { "55612" };
+        private static List<string> sBurnabyStops = new List<string>() { "55612", "51861", "52998", "52807" };
         private static string apiKey = "axidqpmv6rg7WgIOsYjt";
-        private List<string> mStopTimes = new List<string>();
+        private List<BusRoute> mBusRoutes = new List<BusRoute>();
 
         public TransitVM()
         {
             GetTransitTimes();
         }
 
+        public List<BusRoute> BusRoutes
+        {
+            get { return this.mBusRoutes; }
+            set { this.mBusRoutes = value; }
+        }
+
         private void GetTransitTimes()
         {
             foreach(string stop in sBurnabyStops)
             {
-                HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(string.Format("http://api.translink.ca/RTTIAPI/V1/stops/55612?apiKey={0}", apiKey));
+                HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(string.Format("http://api.translink.ca/RTTIAPI/V1/stops/53096/estimates?apiKey={0}", apiKey));
                 request.Method = "GET";
                 request.Accept = "application/json";
                 request.BeginGetResponse(new AsyncCallback(GetStopResponse), request);
@@ -38,10 +47,33 @@ namespace wp8sfu.VMs
             Stream stream = response.GetResponseStream();
             StreamReader reader = new StreamReader(stream);
             string json = reader.ReadToEnd();
-            JObject stopObject = JObject.Parse(json);
+            JArray stopObject = JArray.Parse(json);
+            string routeNo = stopObject[0]["RouteNo"].ToString();
+            string routeName = stopObject[0]["RouteName"].ToString();
 
+            BusRoute route = new BusRoute(routeNo, routeName);
+            JArray times = stopObject[0]["Schedules"] as JArray;
+            foreach(JObject obj in times)
+            {
+                string time = obj["ExpectedLeaveTime"].ToString();
+                route.AddRouteTime(time);
+            }
+            mBusRoutes.Add(route);
+            Deployment.Current.Dispatcher.BeginInvoke(() =>
+                {
+                    OnPropertyChanged("BusRoutes");
+                });
 
+        }
 
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
         }
     }
 }
